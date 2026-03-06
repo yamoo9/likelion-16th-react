310516cd5ddcdda0d4462566d87358da1531cef7
yamoo9 <yamoo9@naver.com>
2026-03-06 13:00:17 +0900

    ready. JSX 학습

--- learing-react/src/main.tsx
+++ learing-react/src/main.tsx
@@ -11,8 +11,17 @@ if (!rootElement) {
   throw new Error('문서에 #root인 요소가 없습니다. 확인해보세요.')
 }
 
-createRoot(rootElement).render(
+// React(Virtual) DOM (가짜 문서 객체 모델의 뿌리)
+const ReactDOMRoot = createRoot(rootElement)
+// console.log(ReactDOMRoot) // ReactDOMRoot { render, unmount }
+ReactDOMRoot.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
+
+// createRoot(rootElement).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
