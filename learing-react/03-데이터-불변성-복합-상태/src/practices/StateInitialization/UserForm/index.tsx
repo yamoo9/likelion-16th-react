d2160569dc8c5fb89fb04270e48bef9abae18d8d
yamoo9 <yamoo9@naver.com>
2026-03-12 09:31:58 +0900

    learn. 제어된 vs 제어되지 않은 상황에서의 폼 제출/초기화
    - 브라우저 기본 작동
    - 리액트에 의해 제어되는 작동

--- learing-react/03-데이터-불변성-복합-상태/src/practices/StateInitialization/UserForm/index.tsx
+++ learing-react/03-데이터-불변성-복합-상태/src/practices/StateInitialization/UserForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, type SubmitEvent } from 'react'
 import S from './style.module.css'
 
 // 인터페이스(Interface) = 객체 형태의 타입 선언
@@ -7,15 +7,49 @@ import S from './style.module.css'
 type UserRole = 'user' | 'admin' | ''
 
 export default function UserForm() {
-  // 사용자 이름(name), 이메일(email), 역할(role)
+  // [상태] 사용자 이름
   const [name, setName] = useState('')
+  // [상태] 이메일
   const [email, setEmail] = useState('')
+  // [상태] 역할
   const [role, setRole] = useState<UserRole>('')
 
   const userRoleKR = role === 'admin' ? '관리자' : '일반 사용자'
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('사용자가 입력한 폼 데이터 확인')
+
+    // [리액트에 의해 제어되지 않는 상황]
+    // 브라우저에 의해 제어되고 있는 네이티브 컨트롤의 경우
+    // 브라우저의 폼 데이터 객체를 통해 확인
+    const formElement = e.currentTarget
+    const formData = new FormData(formElement)
+    console.log(Object.fromEntries(formData))
+
+    // [리액트에 의해 제어되는 상황]
+    // 제어되고 있는 컨트롤(입력 필드, 셀렉트 박스)에 연결된 상태 값을 확인
+    console.log({ name, email, role })
+  }
+
+  const handleReset = (e: SubmitEvent<HTMLFormElement>) => {
+    console.log('사용자가 입력한 폼 데이터 초기화')
+
+    // [리액트에 의해 제어되지 않는 상황]
+    // 브라우저에 의해 제어되고 있는 네이티브 컨트롤의 경우
+    // 브라우저의 기본 초기화 기능으로 처리 가능
+    const formElement = e.currentTarget
+    formElement.reset()
+
+    // [리액트에 의해 제어되는 상황]
+    // 제어되고 있는 컨트롤(입력 필드, 셀렉트 박스)의 초기값을 리액트의 방식으로 처리
+    setName('')
+    setEmail('')
+    setRole('')
+  }
+
   return (
-    <form className={S.container} onSubmit={(e) => e.preventDefault()}>
+    <form className={S.container} onSubmit={handleSubmit} onReset={handleReset}>
       <fieldset>
         <legend>사용자 정보 입력</legend>
 
@@ -61,6 +95,14 @@ export default function UserForm() {
             <option value="user">일반 사용자</option>
             <option value="admin">관리자</option>
           </select>
+        </div>
+
+        <div
+          role="group"
+          style={{ marginBlockStart: 8, display: 'flex', gap: 8 }}
+        >
+          <button type="submit">제출</button>
+          <button type="reset">초기화</button>
         </div>
       </fieldset>
 
--- learing-react/03-데이터-불변성-복합-상태/src/practices/StateInitialization/index.tsx
+++ learing-react/03-데이터-불변성-복합-상태/src/practices/StateInitialization/index.tsx
@@ -6,8 +6,16 @@ import UserForm from './UserForm'
 // 갱신 = 업데이트
 
 export default function StateInitialization() {
+  
   return (
     <div className={S.container}>
+      <header className={S.header}>
+        <h2>회원가입 (버전: {0})</h2>
+        <button type="button" className={S.resetButton}>
+          폼 초기화
+        </button>
+      </header>
+
       <UserForm />
     </div>
   )
