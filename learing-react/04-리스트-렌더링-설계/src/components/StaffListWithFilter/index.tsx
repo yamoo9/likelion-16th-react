ba6030b1fe1d599fd7998a2f3de93661a68ec139
yamoo9 <yamoo9@naver.com>
2026-03-13 11:50:30 +0900

    learn. StaffListWithFilter 필터링 구현 파트 2

--- learing-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
+++ learing-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
@@ -10,11 +10,20 @@ export default function StaffListWithFilter() {
   const [staffs] = useState<Staff[]>(staffData)
   const [search, setSearch] = useState('')
 
+  // 파생된 상태(데이터)
+  // 검색어에 의해 걸러진 스태프 목록
+  const searchedStaffs = staffs.filter((staff) => {
+    const hasName = staff.name.toLowerCase().includes(search.toLowerCase().trim())
+    const hasRole = staff.role.toLowerCase().includes(search.toLowerCase().trim())
+    const hasPhone = staff.phone.toLowerCase().includes(search.toLowerCase().trim())
+    return hasName || hasRole || hasPhone
+  })
+
   return (
     <section className={S.container}>
-      <StaffListInfo staffs={staffs} />
+      <StaffListInfo staffs={staffs} searchedStaffs={searchedStaffs} />
       <StaffListSearch search={search} setSearch={setSearch} />
-      <StaffList staffs={staffs} />
+      <StaffList staffs={searchedStaffs} />
     </section>
   )
 }
--- learing-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/parts/StaffListInfo.tsx
+++ learing-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/parts/StaffListInfo.tsx
@@ -3,14 +3,15 @@ import S from './StaffListInfo.module.css'
 
 interface Props {
   staffs: Staff[]
+  searchedStaffs: Staff[]
 }
 
-export default function StaffListInfo({ staffs }: Props) {
+export default function StaffListInfo({ staffs, searchedStaffs }: Props) {
   return (
     <header className={S.header}>
       <h2>알바생 관리 명부</h2>
       <span className={S.count}>
-        검색된 인원: {staffs.length} / 총원: {staffs.length}명
+        검색된 인원: {searchedStaffs.length} / 총원: {staffs.length}명
       </span>
     </header>
   )
