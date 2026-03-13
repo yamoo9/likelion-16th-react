d582af9f1a1d5a31b0d8289f626d19129d56cf29
yamoo9 <yamoo9@naver.com>
2026-03-13 11:29:26 +0900

    learn. 파생된 상태(Derived State)
    - 리액트의 반응성 데이터(State, Props)가 변경될 때마다 다시 계산되는 데이터

--- learing-react/04-리스트-렌더링-설계/src/components/ProductListWithFilter/index.tsx
+++ learing-react/04-리스트-렌더링-설계/src/components/ProductListWithFilter/index.tsx
@@ -10,17 +10,20 @@ const INITIAL_PRODUCTS: Product[] = productsData
 export default function ProductListWithFilter() {
   // 화면에 렌더링할 데이터 관리 상태 선언
   const [products] = useState(INITIAL_PRODUCTS)
+
   // 검색어 관리할 상태 선언
   const [searchQuery, setSearchQuery] = useState('')
 
-  const filteredData = [
-    {
-      id: 1007767,
-      name: '무드 인센스 홀더 (매트 블랙)',
-      price: 19000,
-      isSoldOut: true,
-    },
-  ]
+  // 상품 데이터를 사용자가 검색한 키워드에 따라 필터링된다면?
+  // ProductSearchResult 컴포넌트는 필터링된 데이터로 화면을 그릴 것이다.
+  // - 자바스크립트 '배열'을 다루는 능력 (filter)
+  // - '파생된 상태(Derived State)'에 대한 이해
+  // - 리액트의 반응성 데이터 (화면을 변경할 수 있는 힘을 가진 데이터: State, Props)
+  // - 반응성 데이터에 의해 반응하는 데이터 (Derived State : 리액트의 상태가 바뀌면 다시 계산된 값)
+
+  const filteredProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  })
 
   return (
     <section className={S.container}>
@@ -29,7 +32,10 @@ export default function ProductListWithFilter() {
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
-      <ProductSearchResult products={filteredData} searchQuery={searchQuery} />
+      <ProductSearchResult
+        products={filteredProducts}
+        searchQuery={searchQuery}
+      />
     </section>
   )
 }
--- learing-react/04-리스트-렌더링-설계/src/components/index.ts
+++ learing-react/04-리스트-렌더링-설계/src/components/index.ts
@@ -9,7 +9,7 @@ export { default as ScheduleTable } from './ScheduleTable'
 // 컴포넌트 분리(추출)
 export { default as ProductList } from './ProductList'
 
-// 데이터 필터링
+// 데이터 필터링 (파생된 데이터 활용)
 export { default as ProductListWithFilter } from './ProductListWithFilter'
 export { default as StaffListWithFilter } from './StaffListWithFilter'
 
