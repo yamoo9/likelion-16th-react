d53555bc64bc4d7a51ab3c11309cd411408407ea
yamoo9 <yamoo9@naver.com>
2026-03-17 15:37:36 +0900

    refactor. 닉네임 검사 보완
    - change 이벤트 핸들러 안에서 e.nativeEvent.isComposing 제거
    - compositionEnd 이벤트 추가
    - blur 이벤트 추가

--- learing-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/NicknameField.tsx
+++ learing-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/NicknameField.tsx
@@ -14,7 +14,6 @@ export default function NicknameField({ value, onChange }: Props) {
   const fieldId = useId()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const nativeEvent = e.nativeEvent as InputEvent
     const { value } = e.target
 
     // TODO 1: [글자 수 제한]
@@ -25,36 +24,18 @@ export default function NicknameField({ value, onChange }: Props) {
       return // 함수 종료
     }
 
-    // TODO 2: [조합 중(IME) 처리]
-    // 한글이 조립 중(isComposing)일 때는 필터링을 건너뛰고 상태만 업데이트하세요.
-    if (nativeEvent.isComposing) {
-      // 비속어 필터링 기능은 한글 조립 중에 작동될 경우
-      // 이상하게 작동될 것이므로 한글 조립 중에는 상태 업데이트만 수행
-      onChange(value)
-      return // 함수 종료
-    }
-
-    // TODO 3: [영문/숫자/조합 완료 시 필터링]
+    onChange(value)
+  }
+  
+  const changeProfanity = (value: string) => {
+    // TODO 2: [영문/숫자/조합 완료 시 필터링]
     // String.prototype.replace와 정규식(Regular Expression, RegExp)을 사용해
-    // 비속어를 '???'로 치환하세요.
-    compositionIME(e.currentTarget.value)
-  }
-
-  const handleCompositionEnd = (e: React.CompositionEvent<HTMLInputElement>) => {
-    console.log('한글 조합 완료')
-    // TODO 4: [최종 확정 필터링]
     // 한글 조합이 완전히 끝나는 시점에 다시 한번 비속어를 걸러주세요.
-    compositionIME(e.currentTarget.value)
-  }
-
-  // 한글 조합이 완료된 시점에 비속어를 대체어 변경 로직 처리 함수
-  const compositionIME = (value: string) => {
-    console.log('비속어를 대체어로 변경')
     const filteredValue = value.replace(
       new RegExp(PROFANITY_PATTERN, 'g'), // /바보|멍청이|또라이/g
       PROFANITY_SUBSTITUTION, // '???'
     )
-
+  
     onChange(filteredValue) // 닉네임 상태 업데이트 요청 (다음번 렌더링에서 반영)
   }
 
@@ -74,7 +55,8 @@ export default function NicknameField({ value, onChange }: Props) {
         placeholder="닉네임을 입력하세요"
         value={value}
         onChange={handleChange}
-        onCompositionEnd={handleCompositionEnd}
+        onCompositionEnd={(e) => changeProfanity(e.currentTarget.value)}
+        onBlur={(e) => changeProfanity(e.target.value)}
       />
     </div>
   )
