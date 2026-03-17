0ce7c7478c5473b6b753ba0c2c297921f7f66bbf
yamoo9 <yamoo9@naver.com>
2026-03-17 13:47:40 +0900

    practice. 사용자 ID로 할 일 검색 Part 4.
    - 사용자 입력 디바운스(Debounce)
    - 응답받지 않은 이전 요청의 경우 중단(Abort)
    - 에러 핸들링 (try...catch)

--- learing-react/05-이펙트-비동기-통신/src/api/getTodos.ts
+++ learing-react/05-이펙트-비동기-통신/src/api/getTodos.ts
@@ -14,13 +14,17 @@ export interface Todo {
   userId: number
 }
 
-export const getTodos = async (userId: string) => {
-  const response = await fetch(`${API_URL}/api/todos?userId=${userId}`)
-  const data: ResponseTodosData = await response.json()
-  return data.todos
+export const getTodos = async (userId: string, options?: RequestInit): Promise<Todo[]> => {
+  try {
+    const response = await fetch(`${API_URL}/api/todos?userId=${userId}`, options)
+    const data: ResponseTodosData = await response.json()
+    return data.todos
+  } catch(error) {
+    throw error instanceof Error ? error : new Error(String(error))
+  }
 }
 
-export const getRandomCompletedTodos = (todos: Todo[]) =>
+export const getRandomCompletedTodos = (todos: Todo[]): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     completed: Math.random() >= 0.5,
--- learing-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
+++ learing-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
@@ -2,22 +2,60 @@ import { useEffect, useState } from 'react'
 import { getRandomCompletedTodos, getTodos, type Todo } from '@/api/getTodos'
 import S from './TodoSearch.module.css'
 
+// 디바운스 시간(ms)
+const DEBOUNCE_TIME = 500 // 500ms (200~300ms)
+
 export default function TodoSearch() {
   const [loading, setLoading] = useState(false)
   const [todos, setTodos] = useState<Todo[]>([])
   const [userId, setUserId] = useState('')
 
+  // TODO 1: 상태 업데이트 디바운싱(Debouncing)
+  // 사용자 입력이 멈춘 후 특정 시간이 지난 다음 상태 업데이트 요청 처리 (잦은 요청 완화: 성능 최적화)
+  
+  // TODO 2: 네트워크 요청 중단(abort) ✅
+  // 이전 요청이 아직 응답되지 않은 경우, 이전 요청을 중단해 불필요한 요청이 발생하지 않도록 조치 (성능 최적화)
+
+  // TODO 3: 에러 핸들링 ✅
+  // 에러가 발생한 상황을 사용자에게 알림 (사용자 경험 최적화)
   useEffect(() => {
+
+    // 아직 응답되지 않은 이전의 네트워크 요청 중단
+    const controller = new AbortController()
+    const { signal } = controller
+    
+    // 데이터 가져오기 비동기 함수 
     const fetchTodos = async () => {
+      console.log('데이터 가져오기 요청 시작!')
+
       if (userId === '') return setTodos([])
-      
+        
       setLoading(true)
-      const todos = await getTodos(userId)
-      setTodos(todos)
-      setLoading(false)
+        
+      try {
+        const todos = await getTodos(userId, { signal })
+        setTodos(todos)
+      } catch(error) {
+        if (error instanceof Error && error.name !== 'AbortError') {
+          console.error('데이터 로드 실패')
+          setTodos([]) // todos 상태 초기화
+        }
+      } finally {
+        if (!signal.aborted) {
+          setLoading(false)
+        }
+      }
     }
 
-    fetchTodos()
+    // 디바운스: DEBOUNCE_TIME 만큼 사용자 입력이 더 없다면 그 때 데이터 가져오기 요청
+    const debounceId = setTimeout(fetchTodos, DEBOUNCE_TIME)
+
+    // 클린업(정리) 함수
+    return () => {
+      controller.abort() // 중복 또는 응답되지 않은 이전 요청 중단
+      clearTimeout(debounceId) // setTimeout으로 설정된 콜백 함수 실행 중단
+    }
+
   }, [userId])
 
   const randomCompletedTodos = getRandomCompletedTodos(todos)
