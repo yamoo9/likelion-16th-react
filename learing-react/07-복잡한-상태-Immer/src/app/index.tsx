41a251891ca7f41fc8fe281c9a0f0d2bdcedca4c
yamoo9 <yamoo9@naver.com>
2026-03-23 07:58:31 +0900

    refactor. ShoppingCart 실습 자료

--- learing-react/07-복잡한-상태-Immer/src/app/index.tsx
+++ learing-react/07-복잡한-상태-Immer/src/app/index.tsx
@@ -1,10 +1,11 @@
-import { TodosCRUD } from '@/components'
+
+import { ShoppingCart } from '@/practices'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <TodosCRUD />
+      <ShoppingCart />
     </div>
   )
 }
