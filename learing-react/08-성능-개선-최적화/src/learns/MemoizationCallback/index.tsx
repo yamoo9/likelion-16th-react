d74d1a8f2e551ab9d921b96c5395289088f3bd17
yamoo9 <yamoo9@naver.com>
2026-03-24 09:24:36 +0900

    learn. Prop으로 함수 전달
    - useCallback 훅으로 메모
    - 참조 동일성 유지 (리렌더 방지)
    - 가장 이상적인 방법 소개

--- learing-react/08-성능-개선-최적화/src/learns/MemoizationCallback/index.tsx
+++ learing-react/08-성능-개선-최적화/src/learns/MemoizationCallback/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState/* , useCallback */ } from 'react'
 import { formatTime } from './util/formatTime'
 import GrandFather from './parts/GrandFather'
 import S from './style.module.css'
@@ -21,15 +21,17 @@ export default function MemoizationCallback() {
   // 일반 함수
   // - 매 렌더링(1초마다)마다 새로운 참조값이 생성됨
   // - 자식이 React.memo를 써도 무조건 리렌더링 유발
-  const incrementCount = () => setCount(count + 1)
-
+  // const incrementCount = () => setCount(count + 1)
+  
   // useCallback (의존성 포함)
   // - count가 변할 때만 새로 생성됨. 1초마다 흐르는 time에는 영향받지 않음
   // - 하지만 count가 변할 때 자식도 같이 리렌더링되는 구조라면 적절함
+  // const incrementCount = useCallback(() => setCount(count + 1), [count])
 
-  // useCallback
+  // useCallback (의존성 비움)
   // - 의존성 배열이 비어있어 빌드 시점에 단 한 번만 생성됨
   // - 어떤 상태 변화에도 참조값이 변하지 않는 가장 강력한 최적화
+  // const incrementCount = useCallback(() => setCount((prev) => prev + 1), [])
 
   // set 함수
   // - 리액트가 이미 메모이제이션을 보장함
@@ -45,7 +47,11 @@ export default function MemoizationCallback() {
       </section>
 
       <div className={S.counterSection}>
-        <GrandFather count={count} onIncreament={incrementCount} />
+        <GrandFather 
+          count={count} 
+          setCount={setCount}
+          // onIncreament={incrementCount} 
+        />
       </div>
     </div>
   )
--- learing-react/08-성능-개선-최적화/src/learns/MemoizationCallback/parts/GrandFather.tsx
+++ learing-react/08-성능-개선-최적화/src/learns/MemoizationCallback/parts/GrandFather.tsx
@@ -10,10 +10,10 @@ const iconSize = 22
 
 interface Props {
   count: number
-  onIncreament: () => void
+  setCount: React.Dispatch<React.SetStateAction<number>>
 }
 
-function GrandFather({ count, onIncreament }: Props) {
+function GrandFather({ count, setCount }: Props) {
 
   console.log('%cGrandFather 렌더링', 'color: #349bf0')
 
@@ -24,7 +24,7 @@ function GrandFather({ count, onIncreament }: Props) {
           <img src={grandFatherImage} alt="" width={iconSize} height={iconSize} />
           그랜 파더 카운트: {count}
         </h2>
-        <button type="button" aria-label='카운트 1증가' onClick={onIncreament}>
+        <button type="button" aria-label='카운트 1증가' onClick={() => setCount(count + 1)}>
           카운트 ↑
         </button>
         <Father />
