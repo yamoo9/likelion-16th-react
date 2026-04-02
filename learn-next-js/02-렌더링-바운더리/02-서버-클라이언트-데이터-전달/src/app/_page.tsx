0fc987277cbd6ec849fae35ec08d17797ea2ef29
yamoo9 <yamoo9@naver.com>
2026-04-02 09:30:20 +0900

    learn. 하이드레이션 불일치 경고 무시 설정
    - suppressHydrationWarning 속성 설정
    - 참고: https://ko.react.dev/reference/react-dom/client/hydrateRoot#suppressing-unavoidable-hydration-mismatch-errors

---
+++ learn-next-js/02-렌더링-바운더리/02-서버-클라이언트-데이터-전달/src/app/_page.tsx
@@ -0,0 +1,36 @@
+import PassingData from '@/_learn/ui/passing-data'
+import { cn } from '@/utils'
+
+export default function HomePage() {
+  
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-center justify-center gap-5',
+        'bg-background min-h-screen',
+      )}
+    >
+      <header>
+        <h1
+          className={cn(
+            'text-foreground text-center text-4xl font-extralight',
+            'selection:bg-foreground selection:text-background',
+          )}
+        >
+          데이터 전달
+          <br />
+          <span
+            lang="en"
+            className="inline-block -translate-y-2.5 text-xl text-slate-500"
+          >
+            Passing Data
+          </span>
+        </h1>
+      </header>
+
+      <main>
+        <PassingData />
+      </main>
+    </div>
+  )
+}
