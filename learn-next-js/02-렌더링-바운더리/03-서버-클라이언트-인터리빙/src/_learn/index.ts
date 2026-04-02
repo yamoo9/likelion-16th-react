85048ca8de7b687965ab9ec3337b8f2d33cf1cbb
yamoo9 <yamoo9@naver.com>
2026-04-02 11:46:05 +0900

    config. 인터리빙 데모(Interleaving Demo) 내보내기 설정

--- learn-next-js/02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/_learn/index.ts
+++ learn-next-js/02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/_learn/index.ts
@@ -1,2 +1,3 @@
 export { default as ClientComponent } from './ui/client-component'
 export { default as ServerComponent } from './ui/server-component'
+export { default as InterleavingDemo } from './interleaving'
--- learn-next-js/02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
+++ learn-next-js/02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
@@ -1,8 +1,8 @@
-import { ClientComponent, ServerComponent } from '@/_learn'
 
 import { cn } from '@/utils'
-import Cart from '@/components/cart' // 서버 컴포넌트
-import Modal from '@/components/modal' // 클라이언트 컴포넌트
+import { ClientComponent, InterleavingDemo, ServerComponent } from '@/_learn'
+// import Cart from '@/components/cart' // 서버 컴포넌트
+// import Modal from '@/components/modal' // 클라이언트 컴포넌트
 
 export default function Page() {
 
@@ -35,12 +35,14 @@ export default function Page() {
       </header>
 
       <main className="flex flex-col gap-5 overflow-hidden transform-3d">
+
+        <InterleavingDemo />
         
         {/* 클라이언트 컴포넌트 (동기 처리, 이벤트 핸들링, 상태 관리 등) */}
-        <Modal>
+        {/* <Modal> */}
           {/* 서버 컴포넌트 (비동기 처리, 사전 렌더링, 서버 데이터 가져오기 등) */}
-          <Cart />
-        </Modal>
+          {/* <Cart /> */}
+        {/* </Modal> */}
 
         <ClientComponent hidden>
           {/* <slot></slot> <- props.children */}
