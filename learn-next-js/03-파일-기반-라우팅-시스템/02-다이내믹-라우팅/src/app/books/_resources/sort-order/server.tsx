ec62644d94b956a317875151f7f1d0e90238757c
yamoo9 <yamoo9@naver.com>
2026-04-06 11:47:18 +0900

    learn. 동적 세그먼트 part 6.
    - 서버 컴포넌트 분리
    - 서버 → 서버 컴포넌트 인터리빙(Interleaving)

---
+++ learn-next-js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/_resources/sort-order/server.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/utils"
+import Link from "next/link"
+
+interface Props {
+  sortKey: 'title' | 'pubdate' | 'isbn'
+  orderBy: 'asc' | 'desc'
+}
+
+/* 이름순, 출판일순, ISBN순 정렬(오름차순,내림차순) 기능 구현 */
+function SortOrder({ sortKey, orderBy }: Props) {
+
+  return (
+    <div className="flex gap-5 rounded-xl border border-slate-400 p-5">
+      {/* 링크? (서버 측 제어) 버튼? (클라이언트 측 제어) */}
+      {/* 서버 컴포넌트는 사용자와 상호작용할 수 없다. */}
+      {/* 서버 컴포넌트는 Link 컴포넌트를 통해 소프트 내비게이션이 가능 (전체가 아닌, 부분만 교체) */}
+      <Link
+        className={cn(
+          'text-foreground/70 hover:text-foreground',
+          sortKey.includes('title') &&
+            orderBy.includes('asc') &&
+            'font-black text-blue-600',
+        )}
+        href="?sortKey=title&orderBy=asc"
+      >
+        이름순 정렬 (오름차순)
+      </Link>
+      <Link
+        className={cn(
+          'text-foreground/70 hover:text-foreground',
+          sortKey.includes('title') &&
+            orderBy.includes('desc') &&
+            'font-black text-blue-600',
+        )}
+        href="?sortKey=title&orderBy=desc"
+      >
+        이름순 정렬 (내림차순)
+      </Link>
+    </div>
+  )
+}
+
+export default SortOrder
