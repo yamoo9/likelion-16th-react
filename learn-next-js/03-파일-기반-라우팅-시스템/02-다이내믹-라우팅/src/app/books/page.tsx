9eb7d6f8ac66c7b20e73dfae3da466547a1961f6
yamoo9 <yamoo9@naver.com>
2026-04-06 11:39:40 +0900

    learn. 동적 세그먼트 part 5.
    - 정렬(오름차순,내림차순) 기능 구현
    - 서버 측 기능 구현 (Link 컴포넌트 사용)

--- learn-next-js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/page.tsx
+++ learn-next-js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/page.tsx
@@ -35,7 +35,6 @@ interface Props {
  * - UI: 페이지네이션 정보(TotalCount, TotalPages, HasNextPage) 계산 및 표시
  */
 export default async function BooksPage({ searchParams }: Props) {
-
   // const orderBy = (await searchParams).orderBy ?? 'desc' // 내림차순
   // const sortKey = (await searchParams).sortKey ?? 'pubdate' // 출간일
   // const page = (await searchParams).page ?? 1 // 1 페이지
@@ -68,6 +67,31 @@ export default async function BooksPage({ searchParams }: Props) {
         description="현재 큐레이션 된 도서 목록입니다. 당신의 인생 책을 찾아보세요."
       />
 
+      {/* 이름순, 출판일순, ISBN순 정렬(오름차순,내림차순) 기능 구현 */}
+      <div className="flex gap-5 rounded-xl border border-slate-400 p-5">
+        {/* 링크? (서버 측 제어) 버튼? (클라이언트 측 제어) */}
+        {/* 서버 컴포넌트는 사용자와 상호작용할 수 없다. */}
+        {/* 서버 컴포넌트는 Link 컴포넌트를 통해 소프트 내비게이션이 가능 (전체가 아닌, 부분만 교체) */}
+        <Link
+          className={cn(
+            'text-foreground/70 hover:text-foreground',
+            sortKey.includes('title') && orderBy.includes('asc') && 'font-black text-blue-600'
+          )}
+          href="?sortKey=title&orderBy=asc"
+        >
+          이름순 정렬 (오름차순)
+        </Link>
+        <Link
+          className={cn(
+            'text-foreground/70 hover:text-foreground',
+            sortKey.includes('title') && orderBy.includes('desc') && 'font-black text-blue-600'
+          )}
+          href="?sortKey=title&orderBy=desc"
+        >
+          이름순 정렬 (내림차순)
+        </Link>
+      </div>
+
       {/* books 리스트 렌더링 */}
       <nav
         aria-label="도서 목록"
@@ -89,8 +113,8 @@ export default async function BooksPage({ searchParams }: Props) {
             >
               {book.title}
               <div>
-                <time className='text-rose-500'>{book.pubdate}</time> /{' '}
-                <span className='text-indigo-500'>{book.isbn}</span>
+                <time className="text-rose-500">{book.pubdate}</time> /{' '}
+                <span className="text-indigo-500">{book.isbn}</span>
               </div>
             </Link>
           )
