3244157c356f803677c79469239251bbbec00680
yamoo9 <yamoo9@naver.com>
2026-04-07 11:31:04 +0900

    learn. 포괄적 세그먼트 part. 3
    - 헤더 헤드라인

--- learn-next-js/03-파일-기반-라우팅-시스템/03-포괄적-세그먼트/src/app/docs/[...subjects]/page.tsx
+++ learn-next-js/03-파일-기반-라우팅-시스템/03-포괄적-세그먼트/src/app/docs/[...subjects]/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { Fragment } from 'react'
-import { LucideChevronRight, LucideHome } from 'lucide-react'
+import { LucideChevronRight, LucideFileText, LucideHome } from 'lucide-react'
 
 import { cn } from '@/utils'
 
@@ -14,33 +14,36 @@ export default async function FundamentalPage({
   const decodedSubjects = subjects.map((subject) => decodeURIComponent(subject))
   // console.log(decodedSubjects)
 
+  // 학습 뎁스(depth)
+  const depth = decodedSubjects.length
+
   // 현재 페이지의 제목
   const lastSubjectTitle = decodedSubjects.at(-1)?.replace(/-/g, ' ')
 
   return (
     <section className="w-full space-y-12">
+      
       {/* 상대 경로 안내 (Breadcrumbs) */}
-      <nav className="flex h-10 items-center gap-x-1 font-bold text-slate-400">
-        <Link
-          href={`/docs/${encodeURIComponent('기초')}`}
-          className={cn(
-            'flex shrink-0 items-center gap-x-1.5',
-            'transition-colors hover:text-blue-600',
-          )}
-        >
-          <LucideHome className="size-4" /> 학습 문서 홈
-        </Link>
+      <nav
+        aria-label="학습 문서 제목 단계"
+        className="flex h-10 items-center gap-x-1 mb-10 font-bold text-slate-400"
+      >
         {decodedSubjects.map((subject, index) => {
-          const isLast = index === decodedSubjects.length - 1
+          const isFirst = index === 0
+          const isLast = index === depth - 1
           const href = `/docs/${subjects.slice(0, index + 1).join('/')}`
 
           return (
             <Fragment key={index}>
-              <LucideChevronRight className="size-4 shrink-0 text-slate-400" />
+              {isFirst ? (
+                <LucideHome className="size-4" />
+              ) : (
+                <LucideChevronRight className="size-4 shrink-0 text-slate-400" />
+              )}
               <Link
                 href={href}
                 className={cn(
-                  'rounded-full border px-4 py-1 transition-all',
+                  'rounded-full border py-1 transition-all px-4',
                   isLast
                     ? 'border-blue-600 bg-blue-600 text-white shadow-md'
                     : 'border-transparent hover:bg-blue-50 hover:text-blue-500',
@@ -53,7 +56,33 @@ export default async function FundamentalPage({
         })}
       </nav>
 
-      <h1 className="text-4xl font-black">{lastSubjectTitle}</h1>
+      {/* 페이지 헤더 */}
+      <header className="space-y-5">
+        <div className={cn(
+          'inline-flex items-center gap-2 rounded-full border border-blue-300 bg-blue-50',
+          'px-3 py-1 text-xs font-black tracking-wide text-blue-600'
+        )}>
+          <LucideFileText className="size-3.5" />
+          <span>학습 단계 {depth} / 3</span>
+        </div>
+        <h1
+          className={cn(
+            'text-5xl font-black tracking-tighter text-slate-900',
+            'md:text-6xl',
+          )}
+        >
+          {lastSubjectTitle}
+        </h1>
+        <p
+          className={cn(
+            'max-w-2xl',
+            'text-xl leading-relaxed font-medium text-slate-500',
+          )}
+        >
+          {lastSubjectTitle}에 대한 심화 학습을 위해 아래 하위 주제를
+          선택하세요.
+        </p>
+      </header>
     </section>
   )
 }
